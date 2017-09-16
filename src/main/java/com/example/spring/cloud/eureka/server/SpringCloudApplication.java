9dd470e22e32a8843b64af03394b1cd6ad16d04e
骆晓啸 <xiao-xiao.luo@utsoft.cn>
2017-09-16 21:08:17 +0800

    增加eureka-consumer

--- src/main/java/com/example/spring/cloud/eureka/server/SpringCloudApplication.java
+++ src/main/java/com/example/spring/cloud/eureka/server/SpringCloudApplication.java
@@ -1,4 +1,4 @@
-package com.example.spring.cloud;
+package com.example.spring.cloud.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/spring/cloud/eureka/server/SpringCloudApplicationTests.java
+++ src/test/java/com/example/spring/cloud/eureka/server/SpringCloudApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.spring.cloud.consul.client;
+package com.example.spring.cloud.eureka.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServerApplicationTests {
+public class SpringCloudApplicationTests {
 
 	@Test
 	public void contextLoads() {
